a90172c67c3c41292e96e8ae6ed056ddf0ba7ef5
Cecilia AP <89456756+names-cecilia@users.noreply.github.com>
2022-09-27 14:06:21 +0700

    Update details.js

---
+++ details.js
@@ -0,0 +1,59 @@
+//first box
+function revealMessage1(){
+    var x = document.getElementById("hiddenMessage1");
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//second box
+function revealMessage2(){
+    var x = document.getElementById("hiddenMessage2");
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//third box
+function revealMessage3(){
+    var x = document.getElementById("hiddenMessage3");
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//fourth box
+function revealMessage4(){
+    var x = document.getElementById("hiddenMessage4");
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//fifth box
+function revealMessage5(){
+    var x = document.getElementById("hiddenMessage5");
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//sixth box
+function revealMessage6(){
+    var x = document.getElementById("hiddenMessage6");
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
